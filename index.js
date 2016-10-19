814c8bb0d5b9b49819367589427b103131d63657
Arseni Prokharchyk <arsenpr2011@gmail.com>
2016-10-20 00:39:31 +0300

    init repo

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || '3000';
+app.use(express.static(__dirname));
+
+app.use('/styles', express.static(__dirname + '/public/assets/styles'));
+app.use('/images', express.static(__dirname + '/public/assets/images'));
+app.use('/fonts', express.static(__dirname + '/public/assets/fonts'));
+
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, function() {
+    console.log('Server is started on port ' + port);
+});
