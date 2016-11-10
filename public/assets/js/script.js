14711983657c4a1a98642d551de817732c5edb7f
Arseni Prokharchyk <arsenpr2011@gmail.com>
2016-11-10 12:23:32 +0300

    add scrolling

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -7,6 +7,11 @@ $( document ).ready(function() {
 		$('.main-header, .main-content-title').removeClass('hide');
 		$('.form-wrapp input').val('');
 		$('.form-wrapp').addClass('hide');
-	})
-   
+	});
+
+    $('.header-arrow-down').click(function() {
+        $('html, body').animate({
+            scrollTop: $("#main-content").offset().top
+        }, 350);
+});
 });
