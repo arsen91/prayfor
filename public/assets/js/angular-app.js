6c1cea956fdd6c7689074e5a5b7c5131ac0da0d4
Egor Suzanovich <egor.suzanovich@gmail.com>
2017-01-04 12:47:12 +0300

    Update angular-app.js

--- public/assets/js/angular-app.js
+++ public/assets/js/angular-app.js
@@ -78,6 +78,7 @@ angular.module('molitva', ['ngRoute'])
                     '<span class="dates">27-28 января</span>' +
                 '</div>' +
                 '<a href="#/knowmore" class="know-more nav-btn">узнать больше</a>' +
+                "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');  ga('create', 'UA-89740605-1', 'auto');  ga('send', 'pageview');</script>" +
             '</nav>'
     })
     .component('registerForm', {
